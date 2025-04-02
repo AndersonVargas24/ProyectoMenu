e7d929df3c6d2b149d01dda9f201811509ac946b
AndersonVargas24 <andenasis123@gmail.com>
2025-04-01 21:57:35 -0500

    Login
    Login casi terminado

--- menu/lib/Autehtentication/login.dart
+++ menu/lib/Autehtentication/login.dart
@@ -9,6 +9,10 @@ class LoginMenu extends StatefulWidget {
 }
 
 class _LoginMenuState extends State<LoginMenu> {
+
+  final username = TextEditingController();
+  final password = TextEditingController();
+  bool isvisible = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +24,23 @@ class _LoginMenuState extends State<LoginMenu> {
               children: [
 
                 //Username
+                SizedBox(
+                  width: 400,
+                  height: 290,
+                  child:               
+                Image.asset(
+                  'lib/assets/LogoLogin.png',
+                  fit: BoxFit.cover,  // Ajusta la imagen al tamaño del contenedor                                       
+                )
+            ),
+                const SizedBox(height: 1),
                 Container(
                   margin: EdgeInsets.all(8),
                   padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(8),
-                    color: const Color.fromARGB(255, 97, 155, 202).withOpacity(.3)),
+                    // ignore: deprecated_member_use
+                    color: const Color.fromARGB(255, 97, 155, 202).withOpacity(.5)),
                   child: TextFormField(
                     decoration: const InputDecoration(
                       icon: Icon(Icons.person),
@@ -36,20 +51,44 @@ class _LoginMenuState extends State<LoginMenu> {
                 ),
 
                 //Password
-                             Container(
-                              margin: EdgeInsets.all(8),
+                 Container(
+                   margin: EdgeInsets.all(8),
                   padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(8),
-                    color: const Color.fromARGB(255, 97, 155, 202).withOpacity(.3)),
+                    // ignore: deprecated_member_use
+                    color: const Color.fromARGB(255, 97, 155, 202).withOpacity(.5)),
                   child: TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
+                    obscureText: !isvisible,
+                    decoration: InputDecoration(
+                      icon: const Icon(Icons.lock),
                       border: InputBorder.none,
                       hintText: "Password",
-                    ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isvisible = !isvisible; //boton de visibilidad contraseña
+                          });
+                        }, icon: Icon(isvisible? Icons.visibility: Icons.visibility_off))),
                   ),
                 ),
+                const SizedBox(height: 10),
+                //Login Button
+                Container(
+                  height: 55,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: const Color.fromARGB(255, 16, 60, 134) 
+                  ),
+                  child: TextButton(
+                    onPressed: () {}, 
+                  child: Text("LOGIN", style: TextStyle(color: Colors.white ),
+                  )),
+                  ),
+
+                  //sing up bottom
+
               ],
             ),
           ),
